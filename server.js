d774c6731bd0f690fe294ea4bbce0cbed95cff1e
Vince Tse <vincetse@users.noreply.github.com>
2020-04-20 21:21:43 +0000

    Use Google OAuth

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 'use strict';
 
 const unleash = require('./lib/server-impl');
+const enableGoogleOauth = require('./examples/google-auth-hook');
 
-unleash.start();
+unleash.start({
+    adminAuthentication: 'custom',
+    preRouterHook: enableGoogleOauth,
+});
