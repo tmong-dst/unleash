b8bc46ac39eaf014bf71d934b363d528a8bb4c4d
Tai Mong <tmong@dstillery.com>
2020-05-19 23:37:08 -0400

    logs

--- server.js
+++ server.js
@@ -3,6 +3,9 @@
 const unleash = require('./lib/server-impl');
 const enableGoogleOauth = require('./examples/google-auth-hook');
 
+
+console.log("Starting Unleashed using GoogleOauth");
+
 unleash.start({
     adminAuthentication: 'custom',
     preRouterHook: enableGoogleOauth,
